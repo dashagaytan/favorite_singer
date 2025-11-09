8e4f356d549d51a6eb242f7ad9b95a9b4b9641d0
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2025-11-09 13:38:55 -0600

    populate singers

--- main.js
+++ main.js
@@ -3,3 +3,17 @@ const singers = [
     {name: "Cher", dob: "May 20, 1946", bestSong: "Believe"},
     {name: "Jon Bon Jovi", dob: "March 2, 1962", bestSong: "Livin' on a Prayer"}
 ];
+
+// load page
+document.addEventListener("DOMContentLoaded", () => {
+    const tbody = document.querySelector("#singers");
+
+    singers.forEach(singer => {
+        const row = document.createElement("tr");
+        row.innerHTML = 
+        `<td>${singer.name}</td>
+        <td>${singer.dob}</td>
+        <td>${singer.bestSong}</td>`
+        tbody.appendChild(row);
+    })
+})
