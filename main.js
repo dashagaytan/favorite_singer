12b70d0a46b779207cd6411e33f3bc91943f392d
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2025-11-09 13:33:23 -0600

    singers array

---
+++ main.js
@@ -0,0 +1,5 @@
+const singers = [
+    {name: "Alex Warren", dob: "September 18, 2000", bestSong: "Ordinary"},
+    {name: "Cher", dob: "May 20, 1946", bestSong: "Believe"},
+    {name: "Jon Bon Jovi", dob: "March 2, 1962", bestSong: "Livin' on a Prayer"}
+];
